6c600fc4795b25c32c0230309ee39886ebc01dd7
ayman586 <2alaoa43@solent.ac.uk>
2026-04-23 12:06:25 +0100

    Delete src directory

--- src/db.ts
+++
@@ -1,8 +0,0 @@
-import Database from "better-sqlite3";
-import path from "path";
-
-const dbPath = path.resolve("database/places.db");
-
-export const db = new Database(dbPath, {
-  verbose: console.log
-});
